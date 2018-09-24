ca386115978bf057513ea2db68f3fab1e16f4ef3
palatej <jean.palate@nbb.be>
2018-09-24 17:25:35 +0200

    RSSF (cont.)

--- R files/testssf.R
+++ R files/testssf.R
@@ -13,18 +13,43 @@ bsm<-jd3_ssf_model()
 
 # create the components and add them to the model
 add(bsm, jd3_ssf_locallineartrend("ll"))
-add(bsm, jd3_ssf_seasonal("s", 12, type="Crude", .01, FALSE))
-add(bsm, jd3_ssf_ar("ar", c(1,-.5), variance = 1, fixedvariance = TRUE))
+add(bsm, jd3_ssf_seasonal("s", 12, type="Crude"))
+add(bsm, jd3_ssf_noise("n", variance = 1, fixed = TRUE))
 # create the equation (fix the variance to 1)
 eq<-jd3_ssf_equation("eq", variance=0, fixed = TRUE)
 add(eq, "ll")
 add(eq, "s")
-add(eq, "ar")
+add(eq, "n")
 add(bsm, eq)
 #estimate the model
 rslt<-estimate(bsm, s)
 print(result(rslt, "parameters"))
 print(result(rslt, "loglikelihood"))
+print(result(rslt, "scalingfactor"))
+
+# or
+
+# create the model
+bsm<-jd3_ssf_model()
+
+# create the components and add them to the model
+add(bsm, jd3_ssf_locallineartrend("ll"))
+add(bsm, jd3_ssf_seasonal("s", 12, type="Crude"))
+add(bsm, jd3_ssf_noise("n"))
+# create the equation (fix the variance to 1)
+eq<-jd3_ssf_equation("eq", variance=0, fixed = TRUE)
+add(eq, "ll")
+add(eq, "s")
+add(eq, "n")
+add(bsm, eq)
+#estimate the model
+rslt<-estimate(bsm, s, concentrated=FALSE)
+print(result(rslt, "parameters"))
+rsltbis<-compute(bsm, s, result(rslt, "parameters"),concentrated=TRUE)
+print(result(rslt, "loglikelihood"))
+print(result(rsltbis, "scalingfactor"))
+print(result(rsltbis, "loglikelihood"))
+
 
 # or
 bsmbis<-jd3_ssf_model()
@@ -43,6 +68,7 @@ add(bsmbis, eq)
 rsltbis<-estimate(bsmbis, s)
 print(result(rsltbis, "parameters"))
 print(result(rsltbis, "loglikelihood"))
+print(result(rsltbis, "scalingfactor"))
 
 
 # or
@@ -60,6 +86,7 @@ add(bsm2, eq)
 rslt2<-estimate(bsm2, s)
 print(result(rslt2, "parameters"))
 print(result(rslt2, "loglikelihood"))
+print(result(rslt2, "scalingfactor"))
 
 
 
@@ -78,7 +105,7 @@ add(model2, jd3_ssf_locallevel("tpi"))
 add(model2, jd3_ssf_ar("cycle", c(1, -.5), fixedar = FALSE, variance= 1, fixedvariance=TRUE, nlags= 5))
 
 # create the equations 
-eq1<-jd3_ssf_equation("eq1", variance=1, fixed = TRUE)
+eq1<-jd3_ssf_equation("eq1", variance=1, fixed = FALSE)
 add(eq1, "tu")
 add(eq1, "cycle", .1, FALSE)
 add(model2, eq1)
@@ -96,12 +123,16 @@ add(eq4, "cycle", .1, FALSE)
 add(model2, eq4)
 
 #estimate the model
-rslt2<-estimate(model2, mdata)
+rslt2<-estimate(model2, mdata, concentrated=FALSE)
 #dictionary(rslt2)
 
 print(result(rslt2, "parameters"))
 print(result(rslt2, "loglikelihood"))
 ts.plot(result(rslt2, "ssf.smoothing.cmp(4)"))
+print(result(rslt2, "scalingfactor"))
+p<-result(rslt2, "parameters")
+rslt2bis<-compute(model2, mdata, p, concentrated=TRUE)
+print(result(rslt2bis, "loglikelihood"))
 
 # create the model X2
 mdatax<-cbind(a[,1], a[,10], a[,3], a[,4], a[,6], a[,7])
@@ -117,7 +148,7 @@ add(model3, jd3_ssf_locallevel("tc", variance = 0, fixed = TRUE))
 add(model3, jd3_ssf_ar("cycle", c(1, -.5), fixedar = FALSE, variance= 1,fixedvariance=TRUE, nlags= 5))
 
 # create the equations 
-eq1<-jd3_ssf_equation("eq1", variance=1, fixed = TRUE)
+eq1<-jd3_ssf_equation("eq1")
 add(eq1, "tu")
 add(eq1, "cycle", .1, FALSE)
 add(model3, eq1)
@@ -145,11 +176,15 @@ add(eq6, "cycle", .1, FALSE, jd3_ssf_loading(1))
 add(model3, eq6)
 
 #estimate the model
-rslt3<-estimate(model3, mdatax)
+rslt3<-estimate(model3, mdatax, 1e-20, concentrated=FALSE)
 
 print(result(rslt3, "parameters"))
-print(result(rslt3, "loglikelihood"))
 lines(result(rslt3, "ssf.smoothing.cmp(6)"), col="red")
+p<-result(rslt3, "parameters")
+rslt3bis<-compute(model3, mdatax, p, concentrated=TRUE)
+print(result(rslt3, "scalingfactor"))
+print(result(rslt3, "loglikelihood"))
+print(result(rslt3bis, "loglikelihood"))
 
 # create the model X
 mdatax<-cbind(a[,1], a[,10], a[,3], a[,4], a[,6], a[,7])
@@ -164,7 +199,7 @@ add(model4, jd3_ssf_locallevel("tb", variance = 0, fixed = TRUE)) # = constant
 add(model4, jd3_ssf_locallevel("tc", variance = 0, fixed = TRUE))
 add(model4, jd3_ssf_ar2("cycle", c(1, -.5), fixedar = FALSE, variance= 1,fixedvariance=TRUE, nlags= 4, nfcasts = 4))
 # create the equations 
-eq1<-jd3_ssf_equation("eq1", variance=1, fixed = TRUE)
+eq1<-jd3_ssf_equation("eq1", 1, TRUE)
 add(eq1, "tu")
 add(eq1, "cycle", .1, FALSE, jd3_ssf_loading(4)) # t
 add(model4, eq1)
@@ -184,20 +219,22 @@ eq5<-jd3_ssf_equation("eq5")
 add(eq5, "tb")
 add(eq5, "cycle", .1, FALSE, jd3_ssf_loading(5))
 add(model4, eq5)
-eq6<-jd3_ssf_equation("eq6")
+eq6<-jd3_ssf_equation("eq6", 1, TRUE)
 add(eq6, "tc")
 add(eq6, "cycle", .1, FALSE, jd3_ssf_loading(c(5,6,7,8), c(1,1,1,1)))
 add(model4, eq6)
 
 #estimate the model
-rslt4<-estimate(model4, mdatax)
+rslt4<-estimate(model4, mdatax, marginal=FALSE, concentrated=TRUE)
 
 print(result(rslt4, "parameters"))
 print(result(rslt4, "loglikelihood"))
 pos<-result(rslt4, "ssf.cmppos")
-lines(result(rslt4, paste("ssf.smoothing.array(",pos[7]+4, ")", sep="")), col="blue")
-lines(result(rslt4, paste("ssf.filtering.array(",pos[7]+4, ")", sep="")), col="magenta")
 
 print(result(rslt4, "parametersname"))
 print(result(rslt4, "scalingfactor"))
 
+lines(result(rslt4, paste("ssf.smoothing.array(",pos[7]+4, ")", sep="")), col="blue")
+lines(result(rslt4, paste("ssf.filtering.array(",pos[7]+4, ")", sep="")), col="magenta")
+
+p<-result(rslt4, "parameters")
