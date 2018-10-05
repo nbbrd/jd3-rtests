6c85c59af4d472a436f539bc52c89d81d81a43f1
Jean Palate <jean.palate@nbb.be>
2018-10-05 15:24:46 +0200

    High freq

--- R files/testhighfreq.R
+++ R files/testhighfreq.R
@@ -7,23 +7,26 @@ source("./R files/jd3_tests.R")
 births<-read.table("./Data/births.txt")
 deaths<-read.table("./Data/deaths.txt")
 edf<-read.table("./Data/edf.txt")
+
 y<-edf[,1]
 
 # sa of daily series with X11
-a<-jd3_x11(y, period=7, multiplicative = FALSE, seas0="S3X15", seas1="S3X15", trendLength = 21)
-a1<-jd3_x11(result(a,"d11"), period=365.25, multiplicative = FALSE, trendLength = 367)
-wa1<-result(a1, "d10")
+a<-jd3_x11(y, period=7, multiplicative = TRUE, seas0="S3X15", seas1="S3X15", trendLength = 9)
+a1<-jd3_x11(result(a,"d11"), period=365.25/12, multiplicative = TRUE, seas0="S3X15", seas1="S3X15", trendLength = 65)
+a2<-jd3_x11(result(a1,"d11"), period=365.25, multiplicative = TRUE, trendLength = 367)
+wa2<-result(a2, "d10")
 
 # The final decomposition is given by
 w<-result(a,"d10")
-t<-result(a1,"d12")
-sa<-result(a1,"d11")
-s<-result(a1,"d10")
-i<-result(a1,"d13")
-x11decomp<-cbind(y,t,sa,w,s,i)
+m<-result(a1,"d10")
+t<-result(a2,"d12")
+sa<-result(a2,"d11")
+s<-result(a2,"d10")
+i<-result(a2,"d13")
+x11decomp<-cbind(y,t,sa,w,m, s,i)
 ##
 
-plot(wa1[2000:2500], type="l")
+plot(wa2[2000:2500], type="l")
 
 a2<-jd3_x11(result(a,"d11"), period=365, multiplicative = FALSE, trendLength = 367)
 wa2<-result(a2, "d10")
@@ -42,15 +45,17 @@ s<-result(b1,"s")
 i<-result(b1,"i")
 stldecomp<-cbind(y,t,sa,w,s,i)
 
-c<-jd3_fractionalAirlineDecomposition(y, period=7, adjust = FALSE)
-c1<-jd3_fractionalAirlineDecomposition(result(c,"sa"), period=365.25, adjust = TRUE)
-wc<-result(c1, "s")
+c<-jd3_fractionalAirlineDecomposition(log(y), period=7, TRUE)
+c1<-jd3_fractionalAirlineDecomposition(result(c,"sa"), period=365.25/12, adjust = FALSE)
+c2<-jd3_fractionalAirlineDecomposition(result(c1,"sa"), period=365.25, adjust = FALSE)
+wc<-result(c2, "s")
 plot(wc[2000:2500], type="l")
 
 # The final decomposition is given by
 w<-result(c,"s")
-t<-result(c1,"t")
-sa<-result(c1,"sa")
-s<-result(c1,"s")
-i<-result(c1,"i")
-seatsdecomp<-cbind(y,t,sa,w,s,i)
+m<-result(c1,"s")
+t<-result(c2,"t")
+sa<-result(c2,"sa")
+s<-result(c2,"s")
+i<-result(c2,"i")
+seatsdecomp<-cbind(y,t,sa,w,m,s,i)
