bf80e0f4bb0c176c602fff23c7aef78acd711fb9
Jean Palate <jean.palate@nbb.be>
2018-10-12 08:21:03 +0200

    High-freq. Examples

--- R files/sace6.R
+++ R files/sace6.R
@@ -1,10 +1,12 @@
 source("./R files/jd3_init.R")
 source("./R files/jd3_fractionalairline.R")
+source("./R files/jd3_x11.R")
+source("./R files/jd3_stl.R")
 source("./R files/jd3_holidays.R")
 
 usclaims<-read.table("./Data/usclaims.txt")
 
-w<-jd3_periodicAirline(usclaims[,2], periods=365.25/7, outliers=c("ao", "ls", "wo"), criticalValue = 5)
+w<-jd3_periodicAirline(usclaims[,2], periods=365.25/7, outliers=c("ao", "ls", "wo"), criticalValue = 6)
 
 print(dictionary(w))
 print(result(w,"outliers"))
@@ -15,17 +17,16 @@ y<-log(edf[,1])
 jhol<-jd3_holidays()
 add(jhol, "NewYear")
 add(jhol, "MayDay")
+add(jhol, "Assumption")
 add(jhol, "AllSaintsDay")
 add(jhol, "Christmas", offset=-1)
 add(jhol, "Christmas")
-add(jhol, "Easter")
 add(jhol, "EasterMonday")
 add(jhol, "Ascension")
 add(jhol, "WhitMonday")
-add(jhol, "Assumption")
 add(jhol, c(7, 14))
 
-hol<-jd3_holidaysMatrix(jhol, "1996-01-01", length = length(y))
+hol<-jd3_holidaysMatrix(jhol, "1996-01-01", length = length(y), type = "SkipSundays")
 d<-jd3_periodicAirline(y, x=hol, periods=c(7, 365.25), outliers=c("ao", "ls", "wo"), criticalValue = 6)
 
 print(result(d,"parameters"))
@@ -47,16 +48,24 @@ seatsdecomp<-cbind(y,t,sa,w,s,i)
 y<-exp(y)
 # sa of daily series with X11
 a<-jd3_x11(y, period=7, multiplicative = TRUE, seas0="S3X15", seas1="S3X15", trendLength = 9)
-a1<-jd3_x11(result(a,"d11"), period=365.25/12, multiplicative = TRUE, seas0="S3X15", seas1="S3X15", trendLength = 65)
-a2<-jd3_x11(result(a1,"d11"), period=365.25, multiplicative = TRUE, trendLength = 367)
+a1<-jd3_x11(result(a,"d11"), period=365.25, multiplicative = TRUE, trendLength = 367)
 
 # The final decomposition is given by
 w<-result(a,"d10")
-m<-result(a1,"d10")
-t<-result(a2,"d12")
-sa<-result(a2,"d11")
-s<-result(a2,"d10")
-i<-result(a2,"d13")
-x11decomp<-cbind(y,t,sa,w,m, s,i)
+t<-result(a1,"d12")
+sa<-result(a1,"d11")
+s<-result(a1,"d10")
+i<-result(a1,"d13")
+x11decomp<-cbind(y,t,sa,w, s,i)
 
+b<-jd3_stl(y, period=7, multiplicative = TRUE, swindow=15, twindow=9)
+b1<-jd3_stl(result(b,"sa"), period=365, multiplicative = TRUE)
+
+# The final decomposition is given by
+w<-result(b,"s")
+t<-result(b1,"t")
+sa<-result(b1,"sa")
+s<-result(b1,"s")
+i<-result(b1,"i")
+stldecomp<-cbind(y,t,sa,w,s,i)
 
